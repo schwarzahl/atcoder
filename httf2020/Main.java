42f74205217f8b227fa683ca30493f62b12e5a73
schwarzahl <schwarzahl131072@gmail.com>
2019-11-02 16:06:35 +0900

    明らかに使われていないやつは削除 4803661点

--- httf2020/Main.java
+++ httf2020/Main.java
@@ -61,6 +61,27 @@ public class Main {
 		Random random = new Random();
 
 		search(gy, gx, order, N, 'G');
+		int[][] changeCount = new int[N][N];
+		for (int i = 0; i < M; i++) {
+			int my = ry[i];
+			int mx = rx[i];
+			char mc = c[i];
+			while (my != gy || mx != gx) {
+				if (order[my][mx] != 'N' && order[my][mx] != mc) {
+					changeCount[my][mx]++;
+					mc = order[my][mx];
+				}
+				my = (my + dir_y[c2dir[mc]] + N) % N;
+				mx = (mx + dir_x[c2dir[mc]] + N) % N;
+			}
+		}
+		for (int y = 0; y < N; y++) {
+			for (int x = 0; x < N; x++) {
+				if (changeCount[y][x] == 0) {
+					order[y][x] = 'N';
+				}
+			}
+		}
 		while (System.currentTimeMillis() - st < LIMIT_TIME) {
 			try_num++;
 		}
